f40b26e4feb8ba1fb7fbb94b11d27a2a08004d48
kaintza <adam.kaintz@gmail.com>
2015-12-15 11:20:56 +0100

    configurating Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-resources :posts
-root "posts#index"
+  devise_for :users, :controllers => { registrations: 'registrations'}
+  resources :posts
+  root "posts#index"
 
 end
